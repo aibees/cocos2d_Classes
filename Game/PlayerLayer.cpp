4558fbe3e98336376f34a6286ce17779a4b3e4d3
aibees <aibees1129@naver.com>
2019-02-19 03:26:05 +0900

    englishize comments

--- Game/PlayerLayer.cpp
+++ Game/PlayerLayer.cpp
@@ -22,7 +22,7 @@ bool PlayerLayer::init() {
 	}
 	auto Visible = Director::getInstance()->getVisibleSize();
 	
-	//---------------- Player 생성  ------------------------------
+	//---------------- Player create ------------------------------
 	player = new Player(Vec2(Visible.width / 2, Visible.height / 2), 100, 100, 100);
 	
 	CCSprite* spritePlayer = player->getSprite();
@@ -32,7 +32,7 @@ bool PlayerLayer::init() {
 		// flag init
 	isMoving = directionFlag = false;
 	
-		//Event Dispatcher 생성 -> listener와 Object를 중매해 주는 역할
+		//Event Dispatcher create -> Have role for connection between listener and Object.
 	EventDispatcher* player_dispatcher = Director::getInstance()->getEventDispatcher();
 	auto playerPosition = EventListenerTouchOneByOne::create();
 	playerPosition->setSwallowTouches(true);
@@ -54,7 +54,7 @@ bool PlayerLayer::init() {
 
 bool PlayerLayer::TouchBegan(Touch* touch, Event* event) {
 	newPosition = touch->getLocation();
-	//방향벡터를 touch하는 순간에 값 계산하고 normalize
+	//At the moment of caculating direct vector, will normalize this.
 	normalVector = Vec2(newPosition.x - player->getPosition().x, newPosition.y - player->getPosition().y);
 	normalVector.normalize();
 	if (newPosition != player->getPosition())
@@ -80,31 +80,30 @@ void PlayerLayer::player_callback(EventCustom* event) {
 }
 
 void PlayerLayer::update(float delta) {
-	// tag로 playerSprite load
+	// playerSprite load with tag value
 	auto playerSprite = static_cast<CCSprite*>(getChildByTag(PLAYER));
 	Vec2 move, d;
 	if (isMoving) {
-		// rotation set : flag 하나를 사용하여 새로운 이동이 감지될 때마다 flag를 세워 rotation 갱신
-		// 그 후에 다시 false로 바꾸어 새로운 direction 갱신이 있을 때까지 rotation 값을 유지
+
+		//rotation set : update rotation whenever detected new move of sprite by flag
+		//after, keep the rotation variable until detecting renewed direction
 		if(directionFlag) {
 			playerSprite->setRotation(player->calc_rotate(newPosition));
 			directionFlag = false;
 		}
+		// delta vector
 		d = Vec2(normalVector.x * delta * player->getMoveSpeed(), normalVector.y * delta * player->getMoveSpeed());
-		// move : 매 frame마다 sprite가 이동할 거리 vector
+		
+		// move : player sprite will move by distance vector at a frame
 		move = Vec2(player->getPosition().x + (d.x), player->getPosition().y + (d.y));
 
-		// player 객체에 position 갱신 후 sprite 위치도 갱신
+		// after updates player object's position, will updates sprite position
 		player->setPosition(move);
 		playerSprite->setPosition(player->getPosition());
-		
-		log("sprite position : %f || %f", player->getPosition().x, player->getPosition().y);
-		log("newposition : %f || %f", newPosition.x, newPosition.y);
 
+		// check moving
 		if (abs(newPosition.x - player->getPosition().x) < abs(d.x) && abs(newPosition.y - player->getPosition().y) < abs(d.y)) {
 			isMoving = false;
 		}
 	}
-	// check moving
-	
 }
