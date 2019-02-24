bdce35dd8c85500ec46cf9f31b55c454783b959f
Aibees <aibees1129@naver.com>
2019-02-24 13:29:55 +0900

    Make to Singleton Object & Create Item Queue

--- Game/Item/ItemSet.cpp
+++ Game/Item/ItemSet.cpp
@@ -1,1 +1,21 @@
 #include "ItemSet.h"
+
+USING_NS_CC;
+using namespace std;
+
+ItemSet* ItemSet::Instance = NULL;
+
+ItemSet* ItemSet::getInstance() {
+	if (!Instance) {
+		Instance = new ItemSet();
+	}
+	return Instance;
+}
+
+ItemSet::ItemSet() {
+	itemQueue.reserve(5);
+}
+
+int ItemSet::getQueueSize() {
+	return itemQueue.size();
+}
--- Game/Item/ItemSet.h
+++ Game/Item/ItemSet.h
@@ -1,15 +1,27 @@
+/*
+	this class have all infor. of item objects
+	if this class get collision event from itemlayer,
+	creates related item object then stores this in itemQueue container 
+*/
+
 #ifndef __ITEMSET_H__
 #define __ITEMSET_H__
 
 #include <vector>
+#include "cocos2d.h"
 #include "Item.h"
 #include "IClear.h"
 #include "ICloaking.h"
 
 class ItemSet {
 private :
-	vector<Item> itemQueue;
+	static ItemSet* Instance;
+	std::vector<Item*> itemQueue;
+	ItemSet();
 public :
+	static ItemSet* getInstance();
+
+	int getQueueSize();
 
 };
 #endif // !__ITEMSET_H__
