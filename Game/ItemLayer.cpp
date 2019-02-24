a8e8953880df694f9ecb80803b46f93a165ab8fa
Aibees <aibees1129@naver.com>
2019-02-24 13:27:37 +0900

    Load item sprite from list

--- Game/ItemLayer.cpp
+++ Game/ItemLayer.cpp
@@ -1,6 +1,10 @@
 #include "ItemLayer.h"
 
+#define ICLEAR "IClear"
+#define ICLOAKING "ICloaking"
+
 USING_NS_CC;
+using namespace std;
 
 Layer* ItemLayer::createLayer() {
 	Layer* itemLayer = ItemLayer::create();
@@ -10,5 +14,43 @@ Layer* ItemLayer::createLayer() {
 bool ItemLayer::init() {
 	if (!Layer::init()) { return false; }
 
+	this->scheduleUpdate();
+	this->schedule(schedule_selector(ItemLayer::createItem), 10.0f);
 	return true;
+}
+
+void ItemLayer::update(float delta) {
+	// get player Rect data from playerLayer through CollisionData Singleton class
+	playerBox = CollisionData::getInstance()->getPlayerBox();
+	log("sprite vector size : %d", ItemSprite.size());
+}
+
+void ItemLayer::createItem(float delta) {
+	srand((unsigned int)time(0));
+	int item_num = 0;
+	// get filename of item at itemlist db
+	string tmp = "", dir = "Item/";
+	ifstream item_List("C:/Users/JUIL/project1/Classes/Game/Item/ItemList.txt");
+	if (item_List.fail())
+		log("itemList file read fail");
+	// check the number of item I can use
+	while (true) {
+		if (item_List.eof()) { break; }
+		getline(item_List, tmp);
+		item_num++;
+	}
+	int r_item = rand() % item_num + 1;
+	item_List.clear();
+	item_List.seekg(0L, ios::beg);
+	//read item list randomly in range
+	for (int i = 0; i < r_item; i++) { getline(item_List, tmp); }
+	setSprite(dir.append(tmp));
+}
+
+void ItemLayer::setSprite(string filename) {
+	CCSprite* itemSprite = CCSprite::create(filename);
+	Vec2 position = Vec2(rand() % 1440, rand() % 720);
+	itemSprite->setPosition(position);
+	this->addChild(itemSprite);
+	ItemSprite.push_back(itemSprite);
 }
--- Game/ItemLayer.h
+++ Game/ItemLayer.h
@@ -1,15 +1,38 @@
+/*
+	this class only creates item sprite. no create item objects
+	just have sprite's position then, when detected collision event,
+	this class just send event data to ItemSet
+
+	but, list of items that can draw in each level exists in ItemSet class
+*/
+
 #ifndef __ITEM_LAYER_H__
 #define __ITEM_LAYER_H__
 
 #include "cocos2d.h"
+#include "Game/CollisionData.h"
+#include <ctime>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 class ItemLayer : public cocos2d::Layer {
 private :
-
+	cocos2d::Rect playerBox;
+		// container for storing item sprite that exist at game scene
+	std::vector<cocos2d::CCSprite*> ItemSprite;
 public :
 	static cocos2d::Layer* createLayer();
 
 	virtual bool init();
+
+	void setSprite(std::string filename);
+
+	//schedul func
+	virtual void update(float delta);
+	void createItem(float delta);
 
 	CREATE_FUNC(ItemLayer);
 };
