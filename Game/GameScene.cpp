32dcda498be65625d5a911f5feac6a59a68ea21a
aibees <aibees1129@naver.com>
2019-02-28 22:04:15 +0900

    Change layer zOrder

--- Game/GameScene.cpp
+++ Game/GameScene.cpp
@@ -1,8 +1,8 @@
 #include "GameScene.h"
 
 #define TAG_BACKGROUND 0
-#define TAG_ITEM 1
-#define TAG_PLAYER 2
+#define TAG_PLAYER 1
+#define TAG_ITEM 2
 #define TAG_BUTTON 3
 
 USING_NS_CC;
@@ -16,8 +16,8 @@ Scene* GameScene::createScene() {
 	Layer* playerLayer = PlayerLayer::createLayer();
 	Layer* buttonLayer = ButtonLayer::createLayer();
 	gameScene->addChild(BG_Layer, 0, TAG_BACKGROUND);
-	gameScene->addChild(itemLayer, 1, TAG_ITEM);
-	gameScene->addChild(playerLayer, 2, TAG_PLAYER);
+	gameScene->addChild(playerLayer, 1, TAG_PLAYER);
+	gameScene->addChild(itemLayer, 2, TAG_ITEM);
 	gameScene->addChild(buttonLayer, 3, TAG_BUTTON);
 	return gameScene;
 }
