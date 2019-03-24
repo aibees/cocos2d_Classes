71e4d9120b8dad633d1f6a27ae0b1b0d3ec36e21
Aibees <aibees1129@naver.com>
2019-03-24 23:03:01 +0900

    Make to Interface class

--- Game/Unit/Enemy/Enemy.cpp
+++ Game/Unit/Enemy/Enemy.cpp
@@ -1,37 +1,9 @@
 #include "Enemy.h"
 
-USING_NS_CC;
-
 Enemy::Enemy() {
-	Enemy(Vec2(0, 0), 100, 100);
+
 }
 
-Enemy::Enemy(Vec2 position, long long int hp, long long int mp)
-{
+Enemy::Enemy(cocos2d::Vec2 position) {
 	Position = position;
-	HP = hp;
-	MP = mp;
-}
-
-Vec2 Enemy::getPosition() { return Position; }
-
-void Enemy::setPosition(Vec2 position) {
-	Position = position;
-}
-
-long long int Enemy::getHP() { return HP; }
-
-void Enemy::setHP(long long int hp) {
-	HP = hp;
-}
-
-long long int Enemy::getMP() { return MP; }
-
-void Enemy::setMP(long long int mp) {
-	MP = mp;
-}
-
-void Enemy::setPlayerData(Rect player) {
-	playerBox = player;
-	return;
 }
--- Game/Unit/Enemy/Enemy.h
+++ Game/Unit/Enemy/Enemy.h
@@ -6,19 +6,17 @@
 class Enemy : public Entity {
 private :
 	cocos2d::Vec2 Position;
-	long long int HP;
-	long long int MP;
-	cocos2d::Rect playerBox;
 public :
 	Enemy();
-	Enemy(cocos2d::Vec2 position, long long int hp, long long int mp);
-	virtual cocos2d::Vec2 getPosition();
-	virtual void setPosition(cocos2d::Vec2 position);
-	virtual long long int getHP();
-	virtual void setHP(long long int hp);
-	virtual long long int getMP();
-	virtual void setMP(long long int mp);
-	virtual void setPlayerData(cocos2d::Rect player);
+	Enemy(cocos2d::Vec2 position);
+	virtual cocos2d::Vec2 getPosition() = 0;
+	virtual void setPosition(cocos2d::Vec2 position) = 0;
+	virtual long long int getHP() = 0;
+	virtual void setHP(long long int hp) = 0;
+	virtual long long int getMP() = 0;
+	virtual void setMP(long long int mp) = 0;
+	virtual void setPlayerData(cocos2d::Rect player) = 0;
+	virtual float calcRotation() = 0;
 };
 
 #endif // !__ENEMY_H__
